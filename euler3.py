8b08e4f2587df357304a484111f2ea3230dea1df
Shristi Seth Gupta <sgshristi21@gmail.com>
2018-06-07 12:49:50 +0530

    python file
    The prime factors of 13195 are 5, 7, 13 and 29.
    What is the largest prime factor of the number 600851475143 ?

---
+++ euler3.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+
+"""
+
+#600851475143
+import math as math
+
+def primeFactors(n):
+    
+    while n % 2 == 0:
+        print (2) 
+        n = n / 2
+     
+    # Print the number of two's that divide
+        
+         
+    # n must be odd at this point
+    # so a skip of 2 ( i = i + 2) can be used
+    for i in range(3,int(math.sqrt(n))+1,2):
+         
+        # while i divides n , print i ad divide n
+        while n % i== 0:
+            print(i)
+            n = n / i
+             
+    # Condition if n is a prime
+    # number greater than 2
+    if n > 2:
+        print (n)
+        
+n = 600851475142
+primeFactors(n)
